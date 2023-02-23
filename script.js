bb34b3cc782a7a1fb3c0ac639221d00f0bb83301
Beatriz <becarolf@gmail.com>
2023-02-23 18:04:48 -0500

    structured the initial logic in a better way

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-let result = document.getElementById("screen");
+let currentNumber = "";
+let operator = "";
+let result = document.getElementById("result");
+
+function addDigit(digit) {
+  currentNumber += digit;
+  result.value = currentNumber;
+}
+
+function clear() {
+  result.value= ""
+}
